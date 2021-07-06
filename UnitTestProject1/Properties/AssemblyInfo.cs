e3b00953462fd546200de913bffa58a4885294b5
Артем Пилигузов <yungfuridashi@gmail.com>
2021-07-06 09:00:03 +0300

    Добавлены Unit-тесты

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec6e44f7-4594-4124-aee7-1404065b84e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyProject;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0], -9,11111111111111);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0] * -1, 9,11111111111111);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[0, 0], 0,666666666666667);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[2, 0], 0,888888888888889);
+        }
+    }
+}
+
+
